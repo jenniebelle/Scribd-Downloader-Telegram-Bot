eae238e5b8cc459de58889ef17d3030e78af2f02
MUSTKEEM AHMAD <46074466+Mustkeem324@users.noreply.github.com>
2025-06-01 12:23:41 +0530

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,15 +1,11 @@
-const { bot, handleMessage } = require('../lib/bot');
+// api/webhook.js
+import { bot } from '../lib/bot';
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   if (req.method === 'POST') {
-    const body = req.body;
-
-    if (body.message) {
-      await handleMessage(body.message);
-    }
-
-    res.status(200).send('OK');
+    bot.processUpdate(req.body);
+    res.status(200).send('Webhook received');
   } else {
     res.status(405).send('Method Not Allowed');
   }
-};
+}
